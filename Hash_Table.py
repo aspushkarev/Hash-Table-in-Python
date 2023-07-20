9d14c047c54c8b3a9133fdd2d82575a1335f4e0a
aspushkarev <psashok@mail.ru>
2023-07-20 18:05:10 +0300

    Added methods get_word and fill_ht

--- Hash_Table.py
+++ Hash_Table.py
@@ -1,3 +1,10 @@
+# pip install random-word
+# pip install tqdm
+from random_word import RandomWords
+from tqdm import tqdm
+import random
+
+
 class Node:
     def __init__(self, key, value):
         self.key = key
@@ -6,9 +13,10 @@ class Node:
 
 
 class HashTable:
-    def __init__(self, capacity=100):
+    def __init__(self, capacity=10):
         self.capacity = capacity
         self.size = 0
+        self.collision = 0
         self.table = [None] * capacity
 
     def __len__(self) -> int:
@@ -20,6 +28,15 @@ class HashTable:
             return True
         except KeyError:
             return False
+
+    def __str__(self) -> str:
+        elements = []
+        for i in range(self.capacity):
+            current = self.table[i]
+            while current:
+                elements.append((current.key, current.value))
+                current = current.next
+        return str(elements)
 
     def is_contains(self, key) -> bool:
         try:
@@ -58,6 +75,7 @@ class HashTable:
             current = self.table[index]
             while current:
                 if current.key == key:
+                    self.collision += 1
                     current.value = value
                     return
                 current = current.next
@@ -95,16 +113,49 @@ class HashTable:
             current = current.next
         raise KeyError(key)
 
+    @staticmethod
+    def get_word():
+        """ метод генерации случайных слов """
+        r = RandomWords()
+        return r.get_random_word()
+
+    def fill_ht(self):
+        """ метод наполнения хеш-таблицы """
+        for _ in tqdm(range(self.capacity - self.size)):
+            key = ht.get_word()
+            value = random.randint(1, self.capacity)
+            self.insert(key, value)
+
 
 if __name__ == '__main__':
-    ht = HashTable()
+    ht = HashTable(10)
 
     ht.insert('bananas', 23)
     ht.insert('apple', 2)
     ht.insert('fruits', 34)
 
-    print('apple' in ht)
-    print('box' in ht)
-    print(ht.is_contains('fruits'))
+    print('My hash table:\n', ht)
+    print('Is \'apple\' in hash table?:\n', 'apple' in ht)
+    print('Is \'box\' in hash table?:\n', 'box' in ht)
+    print('Is \'fruits\' in hash table?:\n', ht.is_contains('fruits'))
+    print('The length of hash table:\n', len(ht))
 
-    print(len(ht))
+    print('Filling hash table...Please wait')
+    ht.fill_ht()
+    print('My hash table:\n', ht)
+    print('The length of hash table:\n', len(ht))
+    print('Count of collisions:\n', ht.collision)
+
+    print('Added \'fruits\' into hash table.')
+    ht.insert('fruits', 3333)
+    print('My hash table:\n', ht)
+    print('The length of hash table:\n', len(ht))
+    print('Count of collisions:\n', ht.collision)
+
+    # Test on collision
+    ht.insert('pencil', 8)
+    ht.insert('cat', 49)
+    ht.insert('man', 15)
+    print('My hash table:\n', ht)
+    print('The length of hash table:\n', len(ht))
+    print('Count of collisions:\n', ht.collision)
